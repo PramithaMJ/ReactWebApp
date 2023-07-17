5f037da6ba7b0d9d0d3db20437cfacf0273c1dc2
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-18 03:13:05 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Main from "./Componets/Main";
 
 function App() {
   return (
@@ -6,8 +7,16 @@ function App() {
     <>
       <h1>PMJ</h1>
       <div>Pramitha Jayasooriy a</div>
+      <Main name="Kasun" city="NEliya" position="Softwear developer" />
+      <Main name="Amal" city="Kurunagala" position="App developer" >
+        <h5>This is under Main components</h5>
+      </Main>
+      <Main name="Amila" city="tangalle" position="web developer" />
     </>
   );
 }
 
 export default App;
+
+
+//STOP 1:42
