211a90ec28b8c9e2d457b4ed0ae2a921b85d6623
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-23 02:01:41 +0530

    create stucture

--- react_js_2/react2/src/Components/App.js
+++ react_js_2/react2/src/Components/App.js
@@ -1,7 +1,26 @@
+import { Fragment } from "react";
+import { myDate } from "../Data/myData";
 import "../Style/App.css";
+import Main from "./Main";
+
+const NewBlock = () => {
+  return (
+    <Fragment>
+      {myDate?.map(({ name, city, age,image }, id) => {
+        return <Main key={id} name={name} city={city}  age={age} image={image}/>;
+      })}
+    </Fragment>
+  );
+};
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Pramitha Jayasooriya</h1>
+      {/* <Main /> */}
+      <NewBlock />
+    </div>
+  );
 }
 
 export default App;
---
+++ react_js_2/react2/src/Components/Main.js
@@ -0,0 +1,25 @@
+function Main({ name, age, city, image }) {
+  return (
+    <div>
+      <h2>This come from main Components.</h2>
+
+      <img
+        src={
+          image
+            ? `$image`
+            : `https://images.pexels.com/photos/207891/pexels-photo-207891.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`
+        }
+        alt="teddy photo"
+      />
+      <div className="main_details">
+        <h3>{!name ? `no-name` : `${name}`}</h3>
+        <p>
+          <span>{city}</span>
+          <span>{age}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
