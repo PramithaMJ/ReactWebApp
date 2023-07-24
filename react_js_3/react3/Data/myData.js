ffd2c7a1f99c6d532e280b6974c9d3509b5276b9
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-25 00:17:31 +0530

    func

---
+++ react_js_3/react3/Data/myData.js
@@ -0,0 +1,32 @@
+export const myData = [
+  {
+    id: 1234,
+    name: "Kasun",
+    city: "Neliya",
+    position: "Web Developer",
+    image:
+      "https://png.pngitem.com/pimgs/s/49-497525_annoyed-peter-peter-family-guy-transparent-hd-png.png",
+  },
+  {
+    id: 1235,
+    name: "Akila",
+    city: "Galle",
+    position: "Developer",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd36Z9hply0yjJg0xjkrcKw0WJ78YcXdw4Ps1UQqvPydcrBbvVmqt9EgRWTV-c5pNWUt4&usqp=CAU",
+  },
+  {
+    id: 1236,
+    name: "pramitha",
+    city: "N-Elia",
+    position: "Engineer",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXG65NGJW2cnVkrxZ31XSmrPhwev-eipjz1TPG62Kxod2Wpv2_3T1fXoxk6I-ArgHS1XA&usqp=CAU",
+  },
+  {
+    id: 1237,
+    name: "Jaysooriya",
+    city: "Kandy",
+    position: "System Engineer",
+  },
+];
---
+++ react_js_3/react3/src/App.js
@@ -0,0 +1,5 @@
+const App =()=>{
+return(<div>HEllo</div>)
+}
+
+export default App;
