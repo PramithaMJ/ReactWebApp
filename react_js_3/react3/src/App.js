f3e13aebdc218057a7d6851e7c3bd5a6f0eea60f
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-25 00:32:34 +0530

    func

--- react_js_3/react3/src/App.js
+++ react_js_3/react3/src/App.js
@@ -1,5 +1,23 @@
-const App =()=>{
-return(<div>HEllo</div>)
-}
+import { useState } from "react";
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [position, setPosition] = useState("");
+
+  return (
+    <div className="main_container">
+      <div className="main_left">
+        <input type="text" value={imageUrl}/>
+        <input type="text" value={name}/>
+        <input type="text" value={city}/>
+        <input type="text" value={position}/>
+        <button>Submit</button>
+      </div>
+      <div className="main_right"></div>
+    </div>
+  );
+};
 
 export default App;
