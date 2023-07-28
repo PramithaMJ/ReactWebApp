074938c30e80a3047e7156853d96af8376caf7cf
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-28 12:03:51 +0530

    condition apply

--- react_js_6/src/App.js
+++ react_js_6/src/App.js
@@ -1,174 +1,28 @@
-import { Fragment, useEffect, useState } from "react";
 import "./App.css";
-import Unit from "./components/Unit";
-import Header from "./components/Header";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  // const [imageUrl, setImageUrl] = useState("");
-  // const [name, setName] = useState("");
-  // const [city, setCity] = useState("");
-  // const [position, setPosition] = useState("");
-
-  const [inputData, setInputData] = useState({
-    imageUrl: "",
-    name: "",
-    city: "",
-    position: "",
-  });
-
-  const [myData, setMyData] = useState([]);
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const changeWindowWidth = () => {
-    setWindowWidth(window.innerWidth);
-  };
+  const [apiId, setApiId] = useState("1");
+  console.log(apiId);
 
   useEffect(() => {
-    window.addEventListener("resize", changeWindowWidth);
-    console.log("use effect calling...");
-
-    return () => {
-      console.log("use effect cleanup fnction running...");
-      window.removeEventListener("resize", changeWindowWidth);
-    };
-  }, [inputData.city]);
-
-  // //when functon rerenderig everytime useeffect will active.
-  // useEffect(() => {
-  //   console.log("use effect calling...");
-  // }, [inputData.name]); //when using [] then useeffect not rerendring.
-  // //when changing the inputData name use Effect is rendering.
-
-  //console.log(myData);
+    console.log("use Effect running");
+  }, [apiId]);
 
   return (
     <div>
-      <Header />
-
-      <div className="main_container">
-        <h1>{windowWidth}</h1>
-        <div className="main_left">
-          <input
-            type="text"
-            value={inputData.imageUrl}
-            placeholder="image url"
-            onChange={(e) => {
-              e.preventDefault();
-              setInputData((preInputData) => ({
-                ...preInputData,
-                imageUrl: e.target.value,
-              }));
-            }}
-          />
-          <input
-            type="text"
-            value={inputData.name}
-            placeholder="name"
-            onChange={(e) => {
-              e.preventDefault();
-              setInputData((preInputData) => ({
-                ...preInputData,
-                name: e.target.value,
-              }));
-            }}
-          />
-          <input
-            type="text"
-            value={inputData.city}
-            placeholder="city"
-            onChange={(e) => {
-              e.preventDefault();
-              setInputData((preInputData) => ({
-                ...preInputData,
-                city: e.target.value,
-              }));
-            }}
-          />
-          <input
-            type="text"
-            value={inputData.position}
-            placeholder="position"
-            onChange={(e) => {
-              e.preventDefault();
-              setInputData((preInputData) => ({
-                ...preInputData,
-                position: e.target.value,
-              }));
-            }}
-          />
-          <button
-            onClick={() => {
-              setMyData((prev) => [
-                ...prev,
-                {
-                  image: inputData.imageUrl,
-                  name: inputData.name,
-                  city: inputData.city,
-                  position: inputData.position,
-                },
-              ]);
-
-              setInputData((prev) => {
-                if (prev.imageUrl.length > 0) {
-                  return {
-                    ...prev,
-                    imageUrl: "",
-                  };
-                } else {
-                  return prev;
-                }
-              });
-
-              setInputData((prev) => {
-                if (prev.name.length > 0) {
-                  return {
-                    ...prev,
-                    name: "",
-                  };
-                } else {
-                  return prev;
-                }
-              });
-
-              setInputData((prev) => {
-                if (prev.city.length > 0) {
-                  return {
-                    ...prev,
-                    city: "",
-                  };
-                } else {
-                  return prev;
-                }
-              });
-
-              setInputData((prev) => {
-                if (prev.position.length > 0) {
-                  return {
-                    ...prev,
-                    position: "",
-                  };
-                } else {
-                  return prev;
-                }
-              });
-            }}
-          >
-            Submit
-          </button>
-        </div>
-        <div className="main_right">
-          {myData?.map(({ image, name, city, position }, index) => (
-            <Unit
-              image={image}
-              name={name}
-              city={city}
-              posistion={position}
-              key={index}
-            />
-          ))}
-        </div>
-      </div>
+      <input
+        type="text"
+        value={apiId}
+        onChange={(e) => {
+          e.preventDefault();
+          const enteredId = e.target.value;
+          if (enteredId.length > 0) {
+            setApiId(enteredId);
+          }
+        }}
+        placeholder="Enter Id "
+      />
     </div>
   );
 };
