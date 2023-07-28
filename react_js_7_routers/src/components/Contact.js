850a006c925c02e8439e6f893f4b4692d252a759
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-28 15:48:07 +0530

    route

--- react_js_7_routers/src/components/Contact.js
+++ react_js_7_routers/src/components/Contact.js
@@ -1,3 +1,34 @@
-const Contact = () => <div>This is ths Contact page </div>;
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+  const [mainData, setMainData] = useState([]);
+  useEffect(() => {
+    const dataFetching = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+      const data = await res.json();
+
+      if (data) {
+        console.log(data);
+        setMainData(data);
+      }
+    };
+    dataFetching();
+  }, []);
+  return (
+    <div>
+      <div>
+        This is ths Contact page
+        {mainData?.map(({ id, title }) => (
+          <Link to={`/contact/${id}`} key={id}>
+            <div>
+              <h3>{title}</h3>
+            </div>
+          </Link>
+        ))}{" "}
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- react_js_7_routers/src/components/User.js
+++ react_js_7_routers/src/components/User.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const User = () => {
   const [data, setData] = useState({});
@@ -33,6 +33,7 @@ const User = () => {
           <p>{data.body}</p>
         </div>
       ) : null}
+      <Link to="/contact">Back</Link>
     </div>
   );
 };
